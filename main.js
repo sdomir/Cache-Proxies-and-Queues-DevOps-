286c043138ab47de4870321dd38b21ac0b0cb8e3
sdomir <sdomir@ncsu.edu>
2016-11-01 20:37:27 -0400

    Added proxy functionality

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var redis = require('redis')
+var http      = require('http');
+var httpProxy = require('http-proxy');
 var multer  = require('multer')
 var express = require('express')
 var fs      = require('fs')
 var app = express()
 // REDIS
 var client = redis.createClient(6379, '127.0.0.1', {})
-
+p = 3000
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
@@ -14,7 +16,6 @@ app.use(function(req, res, next)
 	console.log(req.method, req.url);
 	client.lpush('queue', req.url);
 	
-
 	next(); // Passing the request to the next handler in the stack.
 });
 
@@ -57,7 +58,7 @@ app.get('/meow', function(req, res) {
  })
 
 app.get('/del',function(req,res) {
-	client.del('stack'); 
+	client.del('servers'); 
     	res.send("Emptying list...Done");
 })
 
@@ -90,12 +91,73 @@ app.get('/get', function(req, res) {
 app.get('/', function(req, res) {
   res.send('hello world')
 })
+
+//spawn a new server
+app.get('/spawn', function(req,res) {
+	p = p+1;
+	var server = app.listen(p,function() {
+		var host = server.address().address
+		var port = server.address().port
+		var url = "http://"+host+":"+port
+		client.lpush('servers',url)
+	console.log('New server created at http://%s:%s', host, port)
+	res.send('New server created at '+url)
+	})	
+})
+
+//list all servers
+app.get('/listservers', function(req,res) {
+client.lrange('servers',0,-1, function(err,reply) {
+	res.send(reply);
+	});
+})
+
+//destroy a random server from redis
+app.get('/destroy', function(req,res) {
+var index
+client.llen('servers', function(err,reply) {
+if(reply>1) {
+index=Math.floor(Math.random()*reply)
+//console.log('Removing index:'+reply)
+client.lindex('servers',index, function(err,reply) {
+
+client.lrem('servers',0,reply)
+res.send('Removing:'+reply)
+});
+}
+else {
+res.send('Cannot destroy all servers')
+} 
+});
+
+})
+
 // HTTP SERVER
- var server = app.listen(3000, function () {
+var server = app.listen(3000, function () {
 
    var host = server.address().address
    var port = server.address().port
-
+   var url = "http://"+host+":"+port	
+   //client.lpush('servers',url)
  console.log('Example app listening at http://%s:%s', host, port)
  })
+//Proxy
 
+    var options = {};
+    var proxy   = httpProxy.createProxyServer(options);
+
+    var ser  = http.createServer(function(req, res)
+    {
+      client.rpoplpush('servers','servers', function(err,reply) {
+	console.log('Request handled by:'+reply)	
+      proxy.web( req, res, {target: reply } );
+      //res.send("Request routed to: "+reply);
+	});
+    });
+    ser.listen(8091);
+
+
+
+
+
+//infrastructure.setup();
